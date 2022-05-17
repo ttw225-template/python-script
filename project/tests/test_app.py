0d7677ef50dd6609c9fee40ce2de721b874151ac
TTW <peter86225@gmail.com>
2022-05-17 23:55:04 +0800

    feat(project): example project file and tests

---
+++ project/tests/test_app.py
@@ -0,0 +1,9 @@
+import pytest
+from app import WonderBook
+
+
+class TestWonderBook:
+    @pytest.mark.parametrize("spell, effect", [("Expecto Patronum", True), ("Reducto", False)])
+    def test_patronus_charm(self, spell, effect):
+        spells = WonderBook()
+        assert spells.patronus_charm(spell) == effect
